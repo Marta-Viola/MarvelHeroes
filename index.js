079e92115e6b1f1717bc63ba13e5a25a47214a01
Marta-Viola <marta.manzari02@gmail.com>
2024-11-06 10:51:36 +0100

    back-end parte 2

--- index.js
+++ index.js
@@ -9,32 +9,77 @@ const port = 5500;
 
 //Middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('MarvelHeroes'));
 
 //Connessione a MongoDB
-mongoose.connect('mongodb+srv://smarta:Grace11@cluster0.utcyj.mongodb.net/MarvelHeroes', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect('mongodb+srv://smarta:Grace11@cluster0.utcyj.mongodb.net/MarvelHeroes'
+    ).then(() => {
+        console.log('Connected to MongoDB');
+    }).catch(err => {
+        console.error('Error connecting to MongoDB', err);
+    });
 
 const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    email: String,
-    hero: String
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    hero: {type: String, required: true}
 });
 
 const User = mongoose.model('User', UserSchema);
 
+/*
+const test = new User({
+    username: 'testuser',
+    password: '12345',
+    email: 'teest@example.com',
+    hero: 'Iron MAn'
+});
+test.save().then(() => {
+    console.log('User saved');
+}).catch(err => {
+    console.error('Error saving user', err);
+});
+*/
+
 //Signup
-app.post('/sigup', async (req, res) => {
-    const { username, password, email, hero} = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ username, password: hashedPassword, email, hero});
-    await user.save();
-    res.status(201).send('Utente creato');
+app.get('/signup', (req, res) => {
+    console.log('vaffanculo a Bellandi')
+    res.sendFile('signup.html');
+});
+
+app.post('/signup', async (req, res) => {
+    try {
+        console.log('ciao');
+        const { username, password, email, hero} = req.body;
+        
+        //controlla se l'utente esiste già
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ error: 'Username già utilizzato' });
+        }
+        //cripta la password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        const newUser = new User({ 
+            username, 
+            password: hashedPassword, 
+            email, 
+            hero
+        });
+
+        await newUser.save();
+
+        res.status(201).json({ message: 'Utente creato con successo' });
+    } catch(err) {
+        console.error('Errore nella registrazione', err);
+        res.status(500).json({ error: 'Errore interno del server' });
+    }
 });
 
 //Login
+/*
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
@@ -44,8 +89,9 @@ app.post('/login', async (req, res) => {
     const token = jwt.sign({ username: user.username }, 'segreto', {expiresIn: '1h' });
     res.json({ token });
 });
-
+*/  
 //Middleware di autenticazione
+/*
 const auth = (req, res, next) => {
     const token = req.header('Authorization').replace('Bearer ', '');
     try {
@@ -56,10 +102,7 @@ const auth = (req, res, next) => {
         res.status(401).send('Autenticazione fallita');
     }
 };
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+*/
 
 app.listen(port, () => {
     console.log('Server is running on port ' + port);
